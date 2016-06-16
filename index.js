0767bb2e4100ca064d4d27297dde4fa9d1cd21d2
Demo User <debian@beaglebone.(none)>
2016-06-16 08:06:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var srv = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
+
+srv.start(router.route, handle);
